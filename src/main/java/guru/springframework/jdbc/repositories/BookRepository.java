ee148329678c80bd71cdaa6109b989df47cbda2d
WiiruxCruz <hialf.92@gmail.com>
2022-10-30 17:53:52 -0600

    Null handling

--- src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
+++ src/main/java/guru/springframework/jdbc/repositories/BookRepository.java
@@ -5,7 +5,13 @@ import guru.springframework.jdbc.domain.Book;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.Nullable;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 	Optional<Book> findBookByTitle(String title);
+	Book readByTitle(String title);
+	
+	@Nullable
+	Book getByTitle(@Nullable String title);
+	
 }
---
+++ src/test/java/guru/springframework/jdbc/BookRepositoryTest.java
@@ -0,0 +1,45 @@
+package guru.springframework.jdbc;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ActiveProfiles;
+
+import guru.springframework.jdbc.dao.impl.BookDaoImpl;
+import guru.springframework.jdbc.domain.Book;
+import guru.springframework.jdbc.repositories.BookRepository;
+
+@ActiveProfiles("local")
+@DataJpaTest
+@ComponentScan(basePackages = {"guru.springframework.jdbc.dao"})
+//@Import({BookDaoImpl.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class BookRepositoryTest {
+	@Autowired
+	BookRepository br;
+	
+	@Test
+	void testEmptyResultException() {
+		assertThrows(EmptyResultDataAccessException.class, () -> {
+			Book book = br.readByTitle("foobar4");
+		} );
+	}
+	
+	@Test
+	void testNullParam() {
+		//Book b = br.getByTitle(null);
+		assertNull(br.getByTitle(null));
+	}
+	
+	@Test
+	void testNoException() {
+		assertNull(br.getByTitle("foo"));
+	}
+}
