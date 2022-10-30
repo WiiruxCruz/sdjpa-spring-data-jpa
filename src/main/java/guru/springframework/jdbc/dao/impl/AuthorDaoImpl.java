ed7f54f996ad6eeb7aafcf7e3c78776f808b8248
WiiruxCruz <hialf.92@gmail.com>
2022-10-29 19:21:16 -0500

    Query Methods

--- src/main/java/guru/springframework/jdbc/dao/impl/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/impl/AuthorDaoImpl.java
@@ -25,7 +25,7 @@ public class AuthorDaoImpl implements AuthorDao {
 
     @Override
     public Author findAuthorByName(String firstName, String lastName) {
-        return null;
+        return ar.findAuthorByFirstNameAndLastName(firstName, lastName);
     }
 
     @Override
