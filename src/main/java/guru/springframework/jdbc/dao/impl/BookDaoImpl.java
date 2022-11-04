94b5a2ed066ddeb9482f880e789e526e62decf5e
WiiruxCruz <hialf.92@gmail.com>
2022-11-04 00:56:16 -0600

    Sorting with spring data jpa

--- src/main/java/guru/springframework/jdbc/dao/impl/BookDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/impl/BookDaoImpl.java
@@ -2,6 +2,7 @@ package guru.springframework.jdbc.dao.impl;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
@@ -23,7 +24,9 @@ public class BookDaoImpl implements BookDao {
 	
 	@Override
 	public List<Book> findAllBooksSortByTitle(Pageable pageable) {
-		return null;
+		Page<Book> bookPage = br.findAll(pageable);
+		
+		return bookPage.getContent();
 	}
 	
 	@Override
